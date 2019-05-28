f0f2df808f13e5dc10f3c71db6f0135c85fc246d
panli <n83@qq.com>
2019-05-28 15:55:09 +0800

    语音从gateway server取

--- TVMealTakeAPP/TVAPPweb/src/pages/tvhome/tvhome.ts
+++ TVMealTakeAPP/TVAPPweb/src/pages/tvhome/tvhome.ts
@@ -46,7 +46,7 @@ export class TvhomePage {
   ionViewDidEnter() {
     setTimeout(() => {
       this.playDing();
-      this.playVoice(['语音测试']);
+      this.playVoice('_Test001','语音测试');
     }, 2000);
     this.common.GetStorage(this.common.LSName_curMealName).then(curMealName => this.curMealName = curMealName);
     this.common.GetStorage(this.common.LSName_machineName).then(machineName => this.formValue["machineName"] = machineName);
@@ -68,7 +68,7 @@ export class TvhomePage {
     this.settimeout.clear();
     this.http.Request("getCookedOrders", { minu: 8 }).then(res => {
       let hasNew = false;
-      let newNames = []
+      let newNames = [];
       res.data.lstCall.map(item => {
         let isExist = this.formValue["lstCall"].find(m => { return m.orderid == item.orderid });
         if (isExist) {
@@ -76,14 +76,16 @@ export class TvhomePage {
         } else {
           item["class"] = "blinkAni nameNormal";
           hasNew = true;
-          newNames.push(this.common.getUserCNname(item.username));
+          newNames.push({"usercode":item.usercode,"username":this.common.getUserCNname(item.username)});
         }
       });
       if (hasNew && isFirst == false) {
           this.playDing();
-          setTimeout(() => {
-            this.playVoice(newNames);
-          }, 1000);
+          newNames.map(item=>{
+            setTimeout(() => {
+              this.playVoice(item.usercode,item.username);
+            }, 1000);
+          });
       }
       //console.log(res.data.lstCall);
       this.formValue["lstCall"] = res.data.lstCall;
@@ -164,98 +166,42 @@ export class TvhomePage {
 
   }
 
-  voicePer = 1;
-  playVoice(names) {
-    if (AppConfig.enableVoiceCall == false) return;
+
+  playVoice(usercode,username) {
     /*
-    http://ai.baidu.com/docs#/TTS-API/top
-应用名称 DiningHall
-AppID 16148612
-API Key zszT5NZj22NndwkaUzzDGztl
-Secret Key 8PRq94NQgBm9mlx2idYzaoBmv02EAAjA
-token:30天
-https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=zszT5NZj22NndwkaUzzDGztl&client_secret=8PRq94NQgBm9mlx2idYzaoBmv02EAAjA
-{"access_token":"24.0a7c735f7222db433268adf87ef4cee5.2592000.1559198850.282335-16148612","session_key":"9mzdC3nBs6j46lY4d073G2Y0AVhz5NCfk1HSRYuAomsDE\/V3R8uirN30qj4\/uxSggctfipnixcqq19ghmdzpsS901V2xMw==","scope":"audio_voice_assistant_get brain_enhanced_asr audio_tts_post public brain_all_scope wise_adapt lebo_resource_base lightservice_public hetu_basic lightcms_map_poi kaidian_kaidian ApsMisTest_Test\u6743\u9650 vis-classify_flower lpq_\u5f00\u653e cop_helloScope ApsMis_fangdi_permission smartapp_snsapi_base iop_autocar oauth_tp_app smartapp_smart_game_openapi oauth_sessionkey smartapp_swanid_verify smartapp_opensource_openapi smartapp_opensource_recapi","refresh_token":"25.f68938ce495352f43a6f8d6d2a5a7847.315360000.1871966850.282335-16148612","session_secret":"b85ab8cc2bbcc453205e8dfac4878e18","expires_in":2592000}
-https://tsn.baidu.com/text2audio?tex=请潘立取餐&lan=zh&cuid= 16148612&ctp=1&tok=24.0a7c735f7222db433268adf87ef4cee5.2592000.1559198850.282335-16148612
+    http://172.16.73.144:1788/api/TVMealTake/?_action=AudioTakeMeal&UserCode=GLE0010320&UserName=%E6%BD%98%E7%AB%8B
     */
-    let tok = '24.0a7c735f7222db433268adf87ef4cee5.2592000.1559198850.282335-16148612';
-    let spd = 4;//语速，取值0-15，默认为5中语速
-    let vol = 8;//音量，取值0-15，默认为5中音量
-    let per = this.voicePer;//发音人选择, 0为普通女声，1为普通男生，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女声
-    this.voicePer = this.voicePer == 0 ? 1 : 0;//男女轮播
+    if (AppConfig.enableVoiceCall == false) return;
+    let promiseSrcurl=this.common.GetStorage(this.common.LSName_APIURL).then(apihost => {
+
+      //apihost="172.16.73.140:1788";
+      let srcurl=AppConfig.APIUrlFormat.replace('{url}',apihost).replace('{action}','AudioTakeMeal');
+      srcurl+=`&UserCode=${encodeURIComponent(usercode)}&UserName=${encodeURIComponent(username)}`;
+      return srcurl;
+    });
+    promiseSrcurl.then(srcurl=>{
+      console.log(srcurl);
+      // 初始化Aduio
+      var audio = new Audio();
+      var playPromise;
+      audio.src = srcurl;
+      if (audio.paused) {
+        console.log(audio);
+        playPromise = audio.play();
+        if (playPromise) {
+          playPromise.then(() => {
+
+          }, e => {
+            console.log("play catch", e);
+          });
+        }
+      } else {
+        audio.pause();
+      }
 
 
-    let text = '';
-    if(names.length==1 && names[0]=="语音测试") text=`${names[0]}`;
-    else if (names.length == 1) text = `${names[0]},${names[0]},情取餐`;
-    else if (names.length > 5) {
-      for (let i = 0; i < 5; i++) {
-        text += `${names[i]},`;
-      }
-      text += "情取餐";
-      spd = 6;
-    } else {
-      for (let name of names) {
-        text += `${name},${name},情取餐,`;
-      }
-      spd = 5;
-    }
-    var usrUext = encodeURIComponent(encodeURIComponent(text));
-    var src = `https://tsn.baidu.com/text2audio?tex=${usrUext}&spd=${spd}&vol=${vol}}&per=${per}&lan=zh&cuid=16148612&ctp=1&tok=${tok}`;
-    console.log(src);
-    // 初始化Aduio
-    var audio = new Audio();
-    var playPromise;
-    audio.src = src;
-    if (audio.paused) {
-      console.log(audio);
-      playPromise = audio.play();
-      if (playPromise) {
-        playPromise.then(() => {
-
-        }, e => {
-          console.log("play catch", e);
-        });
-      }
-    } else {
-      audio.pause();
-    }
+    });
+    
   }
-
-  modiUUID() {
-    this.alertCtrl.create({
-     title: `请输入新的UUID`,
-     enableBackdropDismiss: false,
-     message: ``,
-     inputs: [
-       {
-         //type: 'number',
-         name: 'UUID',
-         placeholder: ''
-       },
-     ],
-     buttons: [
-       {
-         text: '取消',
-         handler: data => {
-           console.log('Cancel clicked');
-         }
-       },
-       {
-         cssClass: 'buttonConfirm',
-         text: "确定",
-         handler: data => {
-           let input = data.UUID + "";
-           input = input.trim();
-           if (input.length > 0)
-             this.common.SetStorage(this.common.LSName_UUID, input).then(_ => {
-               //this.ionViewDidEnter();
-             });
-
-         }
-       }
-     ]
-   }).present();
- }
  
 }
