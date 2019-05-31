d414b619535937566110cbfb8cca459447ca15f0
panli <n83@qq.com>
2019-05-31 14:19:24 +0800

    增加使用物理键盘回车或数字键出餐

--- POSCookAPP/src/pages/home/home.ts
+++ POSCookAPP/src/pages/home/home.ts
@@ -38,13 +38,15 @@ export class HomePage {
     this.formValue["lstPaidOreder"] = [];
     this.formValue["lstOrderCount"] = [];
     */
-   this.formValue["lstOreder_unCooked"] = [];
-   this.formValue["lstOrder_Cooked"]= [];
-    this.formValue["unCooked"]=0;
-    this.formValue["Cooked"]=0;
+    this.formValue["lstOreder_unCooked"] = [];
+    this.formValue["lstOrder_Cooked"] = [];
+    this.formValue["unCooked"] = 0;
+    this.formValue["Cooked"] = 0;
   }
 
   ionViewDidEnter() {
+
+    this.regKeyPress(1);
     this.common.GetStorage(this.common.LSName_curMealName).then(curMealName => this.curMealName = curMealName);
     this.common.GetStorage(this.common.LSName_machineName).then(machineName => this.formValue["machineName"] = machineName);
     this.common.GetStorage(this.common.LSName_cantingName).then(cantingName => this.formValue["cantingName"] = cantingName);
@@ -58,6 +60,32 @@ export class HomePage {
     this.loadPaidOrders();
 
 
+
+  }
+  regKeyPress(type) {
+    if (type == 1) {
+      document.onkeypress = (e) => {
+        //let code = String.fromCharCode(e.which);
+        if (e.which >= 49 && e.which <= 57) {//数字1-9
+          let index = parseInt(String.fromCharCode(e.which));
+          console.log(`====>key:${index}`,this.formValue["lstOreder_unCooked"].length );
+          if (this.formValue['lstOreder_unCooked'].length >= index) {
+            let order = this.formValue['lstOreder_unCooked'][index - 1];
+            this.changeStaus(order.orderid, order.iscooked);
+            console.log(`====>key:${index},changeStaus(${order.orderid},${order.iscooked})`);
+          }
+        } else if (e.which == 13) {
+          if (this.formValue['lstOreder_unCooked'].length > 0) {
+            let order = this.formValue['lstOreder_unCooked'][0];
+            this.changeStaus(order.orderid, order.iscooked);
+            console.log(`====>key:Enter,changeStaus(${order.orderid},${order.iscooked})`);
+          }
+        }
+       console.log(`====>key:${e.which}`);
+      };
+    } else {
+      document.onkeypress = (e) => { };
+    }
   }
   loadPaidOrders() {
     this.settimeout.clear();
@@ -79,14 +107,14 @@ export class HomePage {
       this.formValue["unCooked"] =unCooked;
       this.formValue["Cooked"] =Cooked;
       */
-      
+
       this.formValue["lstOreder_unCooked"] = res.data.lstOreder_unCooked;
       this.formValue["lstOrder_Cooked"] = res.data.lstOrder_Cooked;
       this.settimeout.regAction(() => {
         this.loadPaidOrders();
       }, 3000);
-      this.formValue["unCooked"] =res.data.lstOreder_unCooked.length;
-      this.formValue["Cooked"] =res.data.CookedLength;
+      this.formValue["unCooked"] = res.data.lstOreder_unCooked.length;
+      this.formValue["Cooked"] = res.data.CookedLength;
 
 
     }, err => {
@@ -96,7 +124,7 @@ export class HomePage {
       */
 
       this.formValue["lstOreder_unCooked"] = [];
-      this.formValue["lstOrder_Cooked"]= [];
+      this.formValue["lstOrder_Cooked"] = [];
 
       let msgArr = [];
       msgArr.push(err);
@@ -113,8 +141,8 @@ export class HomePage {
     // 清除定时器
     clearInterval(this.interval);
     this.settimeout.clear();
-
-
+    //取消按键监听
+    this.regKeyPress(0);
     console.log("===>homepage leave");
   }
 
@@ -124,28 +152,28 @@ export class HomePage {
   }
 
 
-  changeStaus(orderid,iscooked){
-    console.log(orderid,iscooked);
-    let msg=`是否确定修改状态为[${iscooked==1?"未出餐":"已出餐"}]`;
+  changeStaus(orderid, iscooked) {
+    console.log(orderid, iscooked);
+    let msg = `是否确定修改状态为[${iscooked == 1 ? "未出餐" : "已出餐"}]`;
     //this.common.AlertWithCancel('',()=>{
-      this.common.LoadingShow();
-      this.http.Request("setOrderCooked", {orderid:orderid,cooked:iscooked}).then(res => {
-        this.common.LoadingHide();
-        this.loadPaidOrders();
-  
-      }, err => {
-        this.common.LoadingHide();
-        let msgArr = [];
-        msgArr.push(err);
-        this.common.ShowErrorModal("设置出错", msgArr, 20);
-        
-      });
+    this.common.LoadingShow();
+    this.http.Request("setOrderCooked", { orderid: orderid, cooked: iscooked }).then(res => {
+      this.common.LoadingHide();
+      this.loadPaidOrders();
+
+    }, err => {
+      this.common.LoadingHide();
+      let msgArr = [];
+      msgArr.push(err);
+      this.common.ShowErrorModal("设置出错", msgArr, 20);
+
+    });
     //},()=>{},0,msg,'确定','取消');
   }
 
-  public getUserCNname(userName){
-    let n=userName.indexOf('(') > -1 ? userName.match('\\((.+?)\\)')[1] : userName;
-    if(n.length>10) n=n.substr(0,10);
+  public getUserCNname(userName) {
+    let n = userName.indexOf('(') > -1 ? userName.match('\\((.+?)\\)')[1] : userName;
+    if (n.length > 10) n = n.substr(0, 10);
     return n;
   }
 }
